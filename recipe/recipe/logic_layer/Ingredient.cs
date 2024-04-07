19d95018e5d2435a62ef66ba590f88e79ff3f05f
i526066 <526066@student.fontys.nl>
2024-04-07 23:26:40 +0200

    fixed ingredient class

--- recipe/recipe/logic_layer/Ingredient.cs
+++ recipe/recipe/logic_layer/Ingredient.cs
@@ -9,35 +9,30 @@ namespace logic_layer
     public class Ingredient
     {
         private string name;
-        private decimal quantity;
         private string unit;
         private decimal pricePerUnit;
 
         public string Name { get { return name; } }
-        public decimal Quantity { get { return quantity; } }
         public string Unit { get { return unit; } }
         public decimal PricePerUnit { get { return pricePerUnit; } }
 
-        public Ingredient(string name, decimal quantity, string unit, decimal pricePerUnit)
+        public Ingredient(string name, string unit, decimal pricePerUnit)
         {
             if (string.IsNullOrEmpty(name))
                 throw new ArgumentException("Name cannot be null or empty.");
-
-            if (quantity < 0)
-                throw new ArgumentException("Quantity cannot be negative.");
 
             if (pricePerUnit < 0)
                 throw new ArgumentException("Price per unit cannot be negative.");
 
             this.name = name;
-            this.quantity = quantity;
             this.unit = unit;
             this.pricePerUnit = pricePerUnit;
         }
 
         public decimal CalculateCost()
         {
-            return quantity * pricePerUnit;
+            //add logic
+            return 0;
         }
     }
 }
